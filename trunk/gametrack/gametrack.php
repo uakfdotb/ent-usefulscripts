133a666fc2fba58721ddcc59555da78cac3592a6
perennate@gmail.com <perennate@gmail.com@7e7e50bf-3cff-4299-49f5-bbd647cdeda5>
2012-10-09 17:24:29 +0000

    Added status script, fixed a grammatical error.

--- trunk/gametrack/gametrack.php
+++ trunk/gametrack/gametrack.php
@@ -12,7 +12,7 @@
 #  additionally, they are both stored as comma-delimited strings
 #  to retrieve an array of ID integers, use explode(',', $str)
 
-# it is recommend to set this up as a cronjob every five minutes:
+# it is recommended to set this up as a cronjob every five minutes:
 #    */5 * * * * cd /path/to/gametrack/ && php gametrack.php > /dev/null
 
 # BEGIN CONFIGURATION
